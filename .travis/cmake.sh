071f8052175fc05655b353d9a6381f33c118a9c2
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2017-05-13 22:14:28 -0400

    CMake Travis Install Script - Add popd
    Symmetric with the pushd earlier in the script.

--- .travis/cmake.sh
+++ .travis/cmake.sh
@@ -5,3 +5,4 @@ wget -nc https://cmake.org/files/v3.8/cmake-3.8.1-Linux-x86_64.sh
 chmod 755 cmake-3.8.1-Linux-x86_64.sh
 sed -i -r "s/^interactive=TRUE$/interactive=FALSE/g" cmake-3.8.1-Linux-x86_64.sh
 ./cmake-3.8.1-Linux-x86_64.sh
+popd
