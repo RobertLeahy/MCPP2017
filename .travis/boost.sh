4a391247e8be02dcce3a4437da74260f18a4f1e2
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2017-05-16 03:32:05 +0000

    Travis - Build with Clang 4.0

--- .travis/boost.sh
+++ .travis/boost.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -x
 mkdir boost
 pushd boost
 wget -nc https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.gz
 tar -zxf boost_1_64_0.tar.gz
 pushd boost_1_64_0
 ./bootstrap.sh
-./b2 --toolset=gcc-6 --with-iostreams --with-regex
+if [ ${1} = "clang" ]; then PROPERTIES="cxxflags=-stdlib=libc++ linkflags=-stdlib=libc++"; fi
+./b2 "--toolset=${1}" ${PROPERTIES} --with-iostreams --with-regex
 popd
 popd
