0d61a65f2b57de7e4813c1c397a6d5093896a6e9
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2017-05-17 03:41:24 +0000

    Travis - Build with Boost 1.{63,62,61,60,59,58}.0
    In addition to Boost 1.64.0.

--- .travis/boost.sh
+++ .travis/boost.sh
@@ -2,11 +2,14 @@
 set -x
 mkdir boost
 pushd boost
-wget -nc https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.gz
-tar -zxf boost_1_64_0.tar.gz
-pushd boost_1_64_0
+if [ ${2} = "64" ]; then BOOST_URL="https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.gz"
+else BOOST_URL="https://downloads.sourceforge.net/project/boost/boost/1.${2}.0/boost_1_${2}_0.tar.gz"
+fi
+wget -nc ${BOOST_URL}
+tar -zxf "boost_1_${2}_0.tar.gz"
+pushd "boost_1_${2}_0"
 ./bootstrap.sh
 if [ ${1} = "clang" ]; then PROPERTIES="cxxflags=-stdlib=libc++ linkflags=-stdlib=libc++"; fi
-./b2 "--toolset=${1}" ${PROPERTIES} --with-iostreams --with-regex
+./b2 "--toolset=${1}" ${PROPERTIES} --with-iostreams --with-regex -s NO_BZIP2=1
 popd
 popd
